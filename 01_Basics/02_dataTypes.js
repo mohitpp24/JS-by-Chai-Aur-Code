38d344db64e0b4fe821db4b3180a31f660a00b39
Mohit Patil <mohitpp21@gmail.com>
2025-07-04 06:21:30 +0000

    DataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"user strict"; //treat all js code as newer version
+
+// alert(3 + 3); // we are using nodejs, not browser
+
+console.log(3
+    + 3
+) //code readability should be high
+
+console.log("Mohit");
+
+//DataTypes:
+
+let name = "Mohit";  //String 
+let age = 24; //number
+let isLoggedIn = true; //boolean
+let state = null; //null
+let country; //undefined
+
+//Primitive DataTypes:
+//number => 2 to power 53  //typeof = number
+//bigint => used with bigger values //typeof = function
+//string => always inside "" //typeof = string
+//boolean => true/false //typeof = boolean
+//symbol => shows uniqueness //typeof = function
+//null => standalone value/ representation of empty value //typeof = object
+//undefined => when value is not defined //typeof = undefined
+
+//Non-Primitive DataTypes:
+//objects
+
+console.log(typeof BigInt);
