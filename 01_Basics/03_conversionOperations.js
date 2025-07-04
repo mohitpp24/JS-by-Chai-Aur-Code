01c5723df1cc9737a8c6e842de25e6d8422f256f
Mohit Patil <mohitpp21@gmail.com>
2025-07-04 17:38:06 +0000

    Conversion of DataTypes

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -27,5 +27,49 @@ let stringSomeNum = String(someNum);
 console.log(stringSomeNum);
 console.log(typeof stringSomeNum);
 
+//********************OPERATIONS************************
+
+let value = 3;
+let negativeValue = - value;
+console.log(negativeValue);
+
+// console.log(2+2); //addition
+// console.log(2-2); //subtraction
+// console.log(2*2); //multiplication
+// console.log(2**2);//power
+// console.log(2/2); //division
+// console.log(2%2); //module-gives remainder
+
+
+let str1 = "Hello";
+let str2 = " Mohit";
+let str3 = str1 + str2;
+// console.log(str3); // Hello Mohit
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+// console.log(1 + "2" + 2); //122
+
+// console.log( (1 + 2) * 3 / 3); //use brackets
+
+// console.log(true); //true
+// console.log(+true); //1
+// console.log(true+); //error
+// console.log(+""); //0
+
+let gameCounter = 100;
+gameCounter++; //read mdn docs regarding precidence operators - increment and decrement
+console.log(gameCounter);
+
+
+
+
+
+
+
+
 
 
